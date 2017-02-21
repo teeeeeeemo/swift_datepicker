b7896c44ae1f6446bf1592f2ea53de03401273a0
teeeeeeemo <binooooo@naver.com>
2017-02-21 09:11:25 +0900

    datepicker

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let timeSelector: Selector = #selector(ViewController.updateTime) //타이머가 구동되면 실행할 함수 지정
+    let interval = 1.0 // interval: 간격 1.0 = 1초
+    var count = 0 // 타이머가 설정한 간격대로 실행되는지 확인하기 위한 변수
+    var alarmTime: String?
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+        // 타이머를 설정하기 위한 scheduledTimer함수
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+    
+    func updateTime() {
+//        lblCurrentTime.text = String(count) + "초"
+//        count += 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if(alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
+        
+        
+    }
 
 }
 
