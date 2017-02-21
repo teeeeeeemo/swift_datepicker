f73633d4202458b7627abddaae9baafd24a4c33a
teeeeeeemo <binooooo@naver.com>
2017-02-21 11:10:47 +0900

    교재 173p 답올려주면 확인해보기

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     let timeSelector: Selector = #selector(ViewController.updateTime) //타이머가 구동되면 실행할 함수 지정
+    
+   // let alertSelector: Selector = #selector(ViewController.updateTime)
+    var isAlarmOn = false
+    
+    
     let interval = 1.0 // interval: 간격 1.0 = 1초
     var count = 0 // 타이머가 설정한 간격대로 실행되는지 확인하기 위한 변수
     var alarmTime: String?
@@ -24,6 +29,7 @@ class ViewController: UIViewController {
       
         // 타이머를 설정하기 위한 scheduledTimer함수
         Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,11 +38,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+       
         let datePickerView = sender
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        isAlarmOn = false
         
         formatter.dateFormat = "hh:mm aaa"
         alarmTime = formatter.string(from: datePickerView.date)
@@ -55,12 +63,21 @@ class ViewController: UIViewController {
         formatter.dateFormat = "hh:mm aaa"
         let currentTime = formatter.string(from: date as Date)
         
-        if(alarmTime == currentTime) {
+        if(alarmTime == currentTime && !isAlarmOn) {
             view.backgroundColor = UIColor.red
-        } else {
-            view.backgroundColor = UIColor.white
+           
+            let onTimeAlert = UIAlertController(title: "알림", message: "설정된 시간입니다 !!!", preferredStyle: UIAlertControllerStyle.alert)
+            let onAction = UIAlertAction(title: "네, 알겠습니다.", style: UIAlertActionStyle.default, handler: nil)
+            onTimeAlert.addAction(onAction)
+            present(onTimeAlert, animated: true, completion: {
+                ACTION in self.isAlarmOn = true
+                
+            })
         }
-        
+        else {
+            view.backgroundColor = UIColor.white
+            
+        }
         
     }
 
